f369d2a43fe0c2ae4b2aaa0df43654102ebe673b
Ken Yamada <yamakenth@gmail.com>
2022-01-11 18:11:42 +0700

    Refactor for readability

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 export default function Cart(props) {
-  return(
-    <div className='cart-popup'>
-      {
-        props.showCart? 
-        <div>
-          <div className='cart-page'>Cart Popup</div>
-          <div className='overlay'></div>
-        </div>
-        :
-        <div></div>
-      }
-    </div>
-  );
+  if (props.showCart) {
+    return (
+      <div>
+        <div className='cart-page'>Cart Popup</div>
+        <div className='overlay'></div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }

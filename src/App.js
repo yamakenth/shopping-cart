7b7344a19cc9e970b97c3aa5698d855b1b611bab
Ken Yamada <yamakenth@gmail.com>
2022-01-11 22:51:17 +0700

    Move data directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Data from './data/data.json';
 import Header from './components/Header';
 import Home from './components/Home';
 import Product from './components/Product';

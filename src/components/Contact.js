fa60a47c22352eb4ad96e66f74f85dee0404adab
Ken Yamada <yamakenth@gmail.com>
2022-01-11 22:02:19 +0700

    Add text to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,24 +9,27 @@ import Twitter from '../img/twitter.png';
 export default function Contact() {
   return (
     <div className='contact'>
-      <a href='https://www.github.com/yamakenth' id='github' target='_blank' rel="noreferrer">
-        <img src={GitHub} alt='GitHub' className='contact-logo' />
-      </a>
-      <a href='https://www.facebook.com' target='_blank' rel="noreferrer">
-        <img src={FaceBook} alt='FaceBook' className='contact-logo' />
-      </a>
-      <a href='https://www.instagram.com' target='_blank' rel="noreferrer">
-        <img src={Instagram} alt='Instagram' className='contact-logo' />
-      </a>
-      <a href='https://www.twitter.com' target='_blank' rel="noreferrer">
-        <img src={Twitter} alt='Twitter' className='contact-logo' />
-      </a>
-      <a href='https://www.discord.com' target='_blank' rel="noreferrer">
-        <img src={Discord} alt='Discord' className='contact-logo' />
-      </a>
-      <a href='https://www.tiktok.com' target='_blank' rel="noreferrer">
-        <img src={TikTok} alt='TikTok' className='contact-logo' />
-      </a>
+      <h1>CONTACT US</h1>
+      <div className='contact-logos'>
+        <a href='https://www.github.com/yamakenth' id='github' target='_blank' rel="noreferrer">
+          <img src={GitHub} alt='GitHub' className='contact-logo' />
+        </a>
+        <a href='https://www.facebook.com' target='_blank' rel="noreferrer">
+          <img src={FaceBook} alt='FaceBook' className='contact-logo' />
+        </a>
+        <a href='https://www.instagram.com' target='_blank' rel="noreferrer">
+          <img src={Instagram} alt='Instagram' className='contact-logo' />
+        </a>
+        <a href='https://www.twitter.com' target='_blank' rel="noreferrer">
+          <img src={Twitter} alt='Twitter' className='contact-logo' />
+        </a>
+        <a href='https://www.discord.com' target='_blank' rel="noreferrer">
+          <img src={Discord} alt='Discord' className='contact-logo' />
+        </a>
+        <a href='https://www.tiktok.com' target='_blank' rel="noreferrer">
+          <img src={TikTok} alt='TikTok' className='contact-logo' />
+        </a>
+      </div>
     </div>
   );
 }

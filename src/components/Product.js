4bf5ffa8e7aff00f02e70b44f9cd2a7e4a6eb9a6
Ken Yamada <yamakenth@gmail.com>
2022-01-12 14:51:16 +0700

    Create product display

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// currency formatter 
+const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
 
 export default function Product(props) {
   const coins = props.data;
-  console.log(coins);
 
   return (
     <div className='product'>
       {coins.map((coin) => {
         return (
           <div key={coin.symbol} className='coin-container'>
-            
             <h3>{coin.name}</h3>
-
+            <img src={coin.logo} alt='' className='coin-logo'/>
+            <div className='price-row'>
+              <h4>{coin.symbol.toUpperCase()}</h4>
+              <h4>{formatter.format(coin.price)}</h4>
+            </div>
+            <Link to='/product' className='buy-now'>SHOP NOW</Link>
           </div>
         );
       })}
